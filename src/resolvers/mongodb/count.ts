771492ca3d80adceac1edb08145e991650d81e96
suweigang <suweigang@kingsoft.com>
2018-08-31 10:53:50 +0800

    fix count support EOS 1.2.x

--- src/resolvers/mongodb/count.ts
+++ src/resolvers/mongodb/count.ts
@@ -11,9 +11,11 @@ const getCountResolver = ({
     const now = Date.now();
     const account_controls = await queries.count(mongoClient, "account_controls");
     const accounts = await queries.count(mongoClient, "accounts");
-    const actions = await queries.count(mongoClient, "actions");
+    const action_traces = await queries.count(mongoClient, "action_traces");
+    const block_states = await queries.count(mongoClient, "block_states");
     const blocks = await queries.count(mongoClient, "blocks");
     const pub_keys = await queries.count(mongoClient, "pub_keys");
+    const transaction_traces = await queries.count(mongoClient, "transaction_traces");
     const transactions = await queries.count(mongoClient, "transactions");
 
     const elapsed = Date.now() - now;
@@ -21,9 +23,11 @@ const getCountResolver = ({
     return {
         account_controls,
         accounts,
-        actions,
+        action_traces,
+        block_states,
         blocks,
         pub_keys,
+        transaction_traces,
         transactions,
     };
 };
--- src/typeDefs/mongodb/count.ts
+++ src/typeDefs/mongodb/count.ts
@@ -2,9 +2,11 @@ export const count = `
 type Count {
     account_controls: Int
     accounts: Int
-    actions: Int
+    action_traces: Int
+    block_states: Int
     blocks: Int
     pub_keys: Int
+    transaction_traces: Int
     transactions: Int
 }
 
